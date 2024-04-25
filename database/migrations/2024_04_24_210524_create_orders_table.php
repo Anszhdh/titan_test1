0c2a485cc9096093c9d1e248a574d5f56f9a00ec
Anszhdh <aneszahidah535@gmail.com>
2024-04-25 09:25:20 +0800

    migration database

---
+++ database/migrations/2024_04_24_210524_create_orders_table.php
@@ -0,0 +1,32 @@
+<?php
+use App\Model\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('price',20,2);
+            $table->decimal('total_price',20,2);
+            $table->integer('quantity');
+            $table->string('status',200);
+            $table->foreignIdfor(\App\Models\User::class, 'user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
