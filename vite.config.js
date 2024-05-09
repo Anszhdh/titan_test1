2aab45fc1d4cf32774f9c9e357a853db87fabef5
Anszhdh <aneszahidah535@gmail.com>
2024-05-10 00:58:37 +0800

    homepage

--- vite.config.js
+++ vite.config.js
@@ -16,5 +16,11 @@ export default defineConfig({
                 },
             },
         }),
+        
     ],
+    server: {
+        hmr: {
+            host: 'localhost',
+        },
+    }
 });
